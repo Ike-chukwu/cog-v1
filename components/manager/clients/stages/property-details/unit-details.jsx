47c7e84d7dcbf852d0c9fd9aa57b280fb18bf02f
adedotxn <adedotxn.dev@gmail.com>
2024-05-16 14:25:59 +0100

    changed select to input

--- components/manager/clients/stages/property-details/unit-details.jsx
+++ components/manager/clients/stages/property-details/unit-details.jsx
@@ -16,12 +16,6 @@ const UnitDetails = ({ register, setValue }) => {
           <p className="font-semibold opacity-70">Number of units</p>
 
           <Input
-            {...register("propertyName")}
-            placeholder="Enter Name"
-            className="bg-[#F5F7F9] py-1 px-2 mt-4 placeholder:text-[#B1B1B4] border border-primary rounded-none"
-          />
-
-          <Input
             {...register("numberOfUnits")}
             onChange={(e) => setNumberOfUnits(parseInt(e.target.value))}
             type="number"
